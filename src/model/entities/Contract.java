70fedc89bcd4bdaf5d8f99e29e9e42cbad6db34a
Matheus Costa <matetogamesnew@gmail.com>
2023-10-30 03:33:35 -0300

    implementing entities layer

--- src/model/entities/Contract.java
+++ src/model/entities/Contract.java
@@ -1,4 +1,54 @@
 package model.entities;
 
+//region IMPORTS
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+//endregion
+
+
+
 public class Contract {
+
+    //region ATRIBUTOS
+    private Integer number;
+    private Date date;
+    private Double totalValue;
+    //endregion
+
+    // 1 Contrato pode ter Varias parcelas   relacionamento(1 x *)
+    List<Installment> installmentList = new ArrayList<>();
+
+    //region CONSTRUTORES
+
+    public Contract(){
+
+    }
+
+    public Contract(Integer number, Date date, Double totalValue) {
+        this.number = number;
+        this.date = date;
+        this.totalValue = totalValue;
+    }
+    //endregion
+
+    //region GETTERS
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+
+    //endregion
+
 }
--- src/model/entities/Installment.java
+++ src/model/entities/Installment.java
@@ -1,4 +1,41 @@
 package model.entities;
 
+//region IMPORTS
+import java.util.Date;
+
+
+
+//endregion
+
 public class Installment {
+
+    //region ATRIBUTOS
+    private Date dueDate;
+    private Double amount;
+    //endregion
+
+    //region CONSTRUTORES
+    public Installment(){
+
+    }
+
+    public Installment(Date dueDate, Double amount) {
+        this.dueDate = dueDate;
+        this.amount = amount;
+    }
+    //endregion
+
+    //region GETTERS
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+
+    //endregion
+
 }
